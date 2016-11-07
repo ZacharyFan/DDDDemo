dfea47c7245a3d3918a573246f16e9144e8027f7
ZacharyFan <569426055@qq.com>
2016-11-07 18:06:24 +0800

    review code and add buyservice

--- Mall.Application/BuyService.cs
+++ Mall.Application/BuyService.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using Mall.Domain.IRemoteServices;
+using Mall.Infrastructure.Results;
 
 namespace Mall.Application
 {
     public class BuyService
     {
-        public void Buy(Guid userId, Guid productId)
-        {
+        private readonly IUserService _userService;
+        private readonly IProductService _productService;
 
+        public BuyService(IUserService userService, IProductService productService)
+        {
+            this._userService = userService;
+            this._productService = productService;
         }
+
+        //public Result Buy(Guid userId, Guid productId)
+        //{
+        //    var user = _userService.GetUser(userId);
+        //    if (user == null)
+        //    {
+        //        return Result.Fail("未找到用户信息");
+        //    }
+
+        //    var product = _productService.GetProduct(productId);
+        //    if(product == null)
+        //    {
+        //        return Result.Fail("未找到产品信息");
+        //    }
+
+        //    return null;
+        //}
     }
 }
---
+++ Mall.Infrastructure/Results/Result.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Mall.Infrastructure.Results
+{
+    public class Result
+    { 
+        public Boolean IsSuccess { get; set; }
+
+        public String Msg { get; set; }
+
+        protected Result()
+        {
+        }
+
+        public static Result Fail(String msg)
+        {
+            return new Result
+            {
+                IsSuccess = false,
+                Msg = msg
+            };
+        }
+
+        public static Result Success(String msg)
+        {
+            return new Result
+            {
+                IsSuccess = true,
+                Msg = msg
+            };
+        }
+        
+        public static Result Success()
+        {
+            return Success(string.Empty);
+        }
+    }
+
+
+    public class Result<T> : Result
+    {
+        /// <summary>数据
+        /// </summary>
+        public T Data { get; set; }
+
+        private Result() { }
+
+        /// <summary>失败
+        /// </summary>
+        /// <param name="msg">定义失败信息</param>
+        /// <returns></returns>
+        public new static Result<T> Fail(String msg)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Msg = msg
+            };
+        }
+        /// <summary>失败 
+        /// </summary>
+        /// <param name="data">要返回的数据泛型</param>
+        /// <param name="msg">定义失败信息</param>
+        /// <returns></returns>
+        public static Result<T> Fail(T data, String msg)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Msg = msg,
+                Data = data
+            };
+        }
+
+        /// <summary>成功 
+        /// </summary>
+        /// <param name="data">要返回的数据泛型</param>
+        /// <param name="msg">可定义成功信息</param>
+        /// <returns></returns>
+        public static Result<T> Success(T data, String msg)
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Msg = msg,
+                Data = data
+            };
+        }
+
+        /// <summary>成功 
+        /// </summary>
+        /// <param name="data">要返回的数据泛型</param>
+        /// <returns></returns>
+        public static Result<T> Success(T data)
+        {
+            return Success(data, string.Empty);
+        }
+    }
+}
