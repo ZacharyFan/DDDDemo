b6a179822607869bf8c1bc25b8c79de723222660
ZacharyFan <569426055@qq.com>
2016-11-20 21:48:37 +0800

    add unittest

---
+++ UnitTest.Mall.Domain/CartItemTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Mall.Domain.Aggregate;
+using Mall.Domain.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Mall.Domain
+{
+    [TestClass]
+    public class CartItemTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyQuantity_LessZero_ThrowArgumentException()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(1, cartItem.Quantity);
+            cartItem.ModifyQuantity(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyQuantity_EqualsZero_ThrowArgumentException()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(1, cartItem.Quantity);
+            cartItem.ModifyQuantity(0);
+        }
+
+        [TestMethod]
+        public void ModifyQuantity_MoreZero_Success()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(1, cartItem.Quantity);
+            cartItem.ModifyQuantity(10);
+            Assert.AreEqual(10, cartItem.Quantity);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyPrice_LessZero_ThrowArgumentException()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(100, cartItem.Price);
+            cartItem.ModifyPrice(-1);
+        }
+
+        [TestMethod]
+        public void ModifyPrice_EqualsZero_Success()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(100, cartItem.Price);
+            cartItem.ModifyPrice(0);
+            Assert.AreEqual(0, cartItem.Price);
+        }
+
+        [TestMethod]
+        public void ModifyPrice_MoreZero_Success()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            var cartItem = cart.GetCartItem(new Guid("11111111-1111-1111-1111-111111111111"));
+            Assert.AreNotEqual(null, cartItem);
+            Assert.AreEqual(100, cartItem.Price);
+            cartItem.ModifyPrice(10);
+            Assert.AreEqual(10, cartItem.Price);
+        }
+    }
+}
---
+++ UnitTest.Mall.Domain/CartTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Mall.Domain.Aggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Mall.Domain
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_CartIdDefault_ThrowArgumentException()
+        {
+            var cart = new Cart(default(Guid), Guid.NewGuid(), DateTime.Now);
+            Assert.AreNotEqual(null, cart);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_UserIdDefault_ThrowArgumentException()
+        {
+            var cart = new Cart(Guid.NewGuid(), default(Guid), DateTime.Now);
+            Assert.AreNotEqual(null, cart);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_LastChangeTimeDefault_ThrowArgumentException()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), default(DateTime));
+            Assert.AreNotEqual(null, cart);
+        }
+
+        [TestMethod]
+        public void AddCartItem_NotExisted_TotalItemCountIsIncreased()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            Assert.AreEqual(1, cart.TotalItemCount());
+
+            cart.AddCartItem(new Guid("22222222-2222-2222-2222-222222222222"), 1, 100);
+            Assert.AreEqual(2, cart.TotalItemCount());
+        }
+
+        [TestMethod]
+        public void AddCartItem_Existed_TotalItemCountIsNotIncreasedTotalItemNumIsIncreased()
+        {
+            var cart = new Cart(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            Assert.AreEqual(1, cart.TotalItemCount());
+            Assert.AreEqual(1, cart.TotalItemNum());
+
+            cart.AddCartItem(new Guid("11111111-1111-1111-1111-111111111111"), 1, 100);
+            Assert.AreEqual(1, cart.TotalItemCount());
+            Assert.AreEqual(2, cart.TotalItemNum());
+        }
+    }
+}
